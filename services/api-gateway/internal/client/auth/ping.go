925886b70d558b4e64cb3c65ab7e07c217a76ba2
Danil <danil.li24x@gmail.com>
2025-03-06 21:54:43 +0500

    chores

--- services/api-gateway/internal/client/auth/ping.go
+++ services/api-gateway/internal/client/auth/ping.go
@@ -3,13 +3,16 @@ package authclient
 import "google.golang.org/grpc/connectivity"
 
 func (c *AuthClient) Ping() bool {
-	state := c.conn.GetState()
-	c.log.Infof("Current connection state: %s", state)
+	c.log.Infof("Current connection state: %s", c.State())
 
-	if state == connectivity.Idle {
+	if c.State() == connectivity.Idle {
 		c.log.Warn("Connection is IDLE, attempting to wake it up...")
 		c.conn.Connect() // Forces transition from IDLE to CONNECTING/READY
 	}
 
-	return c.conn.GetState() == connectivity.Ready
+	return c.State() == connectivity.Ready
+}
+
+func (c *AuthClient) State() connectivity.State {
+	return c.conn.GetState()
 }
