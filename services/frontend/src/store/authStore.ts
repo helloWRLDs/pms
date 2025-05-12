ecad1bcc10c0dfdac6fc35ebf3b4ea9b84f26a74
Danil <danil.li24x@gmail.com>
2025-05-12 11:45:57 +0500

    refactor: frontend store

--- services/frontend/src/store/authStore.ts
+++ services/frontend/src/store/authStore.ts
@@ -1,65 +1,39 @@
-import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { isTokenValid } from "../utils/jwt";
-import { AuthData } from "../lib/user";
+import { create } from "zustand";
+import { AuthData } from "../lib/user/session";
+import { persist } from "zustand/middleware";
+import { LocalStorageKeysMap } from "../lib/consts/localstorage";
+import { jwtDecode } from "jwt-decode";
 
-// Local Storage Key
-const AUTH_KEY = "auth";
+interface AuthStore {
+  auth: AuthData | null;
+  isAuthenticated: () => boolean;
+  setAuth: (auth: AuthData) => void;
+  clearAuth: () => void;
+}
 
-// Load initial state from localStorage
-const loadInitialState = (): AuthData => {
-  const nullState: AuthData = {};
-  try {
-    const data = localStorage.getItem(AUTH_KEY);
-    if (!data) {
-      return nullState;
+export const useAuthStore = create<AuthStore>()(
+  persist(
+    (set, get) => ({
+      auth: null,
+      isAuthenticated: () => {
+        const auth = get().auth;
+        if (!auth) {
+          return false;
+        }
+        if (!auth.access_token) {
+          return false;
+        }
+        const decoded = jwtDecode(auth.access_token);
+        if (!decoded.exp || decoded.exp > Date.now()) {
+          return false;
+        }
+        return true;
+      },
+      setAuth: (auth: AuthData) => set({ auth }),
+      clearAuth: () => set({ auth: null }),
+    }),
+    {
+      name: LocalStorageKeysMap.AUTH,
     }
-    const state: AuthData = JSON.parse(data);
-    if (!isTokenValid(state.access_token || "")) {
-      return nullState;
-    }
-    return state;
-  } catch (e) {
-    console.log(`Failed loading initial auth state: ${e}`);
-    return nullState;
-  }
-};
-
-const initialState = loadInitialState();
-
-// Slice
-const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    login: (state: AuthData, action: PayloadAction<AuthData>) => {
-      state.access_token = action.payload.access_token;
-      state.user = action.payload.user;
-      state.session_id = action.payload.session_id;
-      state.selected_company_id = action.payload.selected_company_id;
-      state.exp = action.payload.exp;
-      localStorage.setItem(AUTH_KEY, JSON.stringify(state));
-    },
-    logout: () => {
-      localStorage.removeItem(AUTH_KEY);
-      return {};
-    },
-    updateAuthField: (state, action: PayloadAction<Partial<AuthData>>) => {
-      Object.assign(state, action.payload);
-      localStorage.setItem(AUTH_KEY, JSON.stringify(state));
-    },
-  },
-});
-
-// Actions
-export const { login, logout, updateAuthField } = authSlice.actions;
-
-// Store
-export const authStore = configureStore({
-  reducer: {
-    auth: authSlice.reducer,
-  },
-});
-
-// Types
-export type RootState = ReturnType<typeof authStore.getState>;
-export type AppDispatch = typeof authStore.dispatch;
+  )
+);
